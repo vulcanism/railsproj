b45acfc85081a140feb11040ddd5884dd935ea7a
Savannah C <cynomania@gmail.com>
2019-09-18 16:21:04 -0400

    fixed typos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,11 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save
-         session[:user_id] = @user.id
-         redirect_to user_path(@user)
-        else
-         render :new
+            if @user.save
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                redirect_to root_path
         end 
     end
 
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, :password)
+        params.require(:user).permit(:username, :email, :password)
     end
 
 
