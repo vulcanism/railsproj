2a9f0bd7df1cbfe7964fb9ce01315419e2d8153e
Savannah C <cynomania@gmail.com>
2019-09-18 16:08:25 -0400

    user and session controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,17 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(email: params[:user][:email])
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id 
+                redirect_to user_path(@user)
+            else                
+                redirect_to login_path
+            end
     end
 
     def destroy
+        session.clear
+        redirect_to root_path
     end
 end
