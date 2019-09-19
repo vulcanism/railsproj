60f9be6f41c476ad264e944d60c1ffe40a2ac61d
Savannah C <cynomania@gmail.com>
2019-09-19 12:18:13 -0400

    created view files, added helper methods

--- app/controllers/cryptids_controller.rb
+++ app/controllers/cryptids_controller.rb
@@ -1,3 +1,22 @@
 class CryptidsController < ApplicationController
 
+    def index
+    end
+    
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    private
+
+    def cryptid_params
+        params.require(:cryptid).permit(:name, :location, :size, :type)
+    end
+
+
 end
