eff2cfaf8aea7efa3f54ff242de3c1bc9907651e
Savannah C <cynomania@gmail.com>
2019-09-22 18:14:09 -0400

    omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+    provider :github, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'],
+    :redirect_uri => ENV['GITHUB_REDIRECT']
 end
