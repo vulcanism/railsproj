60a2e8aa37cf8fa21a9ad6bbdd76b8304e802b19
Savannah C <cynomania@gmail.com>
2019-09-26 15:47:58 -0400

    migrated comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_211025) do
+ActiveRecord::Schema.define(version: 2019_09_26_194225) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "cryptid_id"
+    t.index ["cryptid_id"], name: "index_comments_on_cryptid_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "cryptids", force: :cascade do |t|
     t.string "name"
