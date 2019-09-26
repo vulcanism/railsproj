beabff928e3d57ee24244aa108562de730f73610
Savannah C <cynomania@gmail.com>
2019-09-26 15:47:00 -0400

    built out create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,14 @@
 class CommentsController < ApplicationController
 
+    def create
+        @comment = Comment.create(comment_params)
+        redirect_to comment.cryptid
+    end
+
+    private
+
+    def comment_params
+        params.require(:content).permit(:content, :user_id, :cryptid_id)
+    end
+
 end
